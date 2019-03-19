419e3e813d446d2beb5c3a6ede835b7eb4ca8262
Jeril George Kadavan <jerilgkadavan@gmail.com>
2019-03-19 17:25:55 -0400

    media query

--- data.js
+++ data.js
@@ -1,18 +1,29 @@
 const data = {
 
-    title:"Beats Studio3 Wireless Headphones",
-    desOne:"The Beats Studio3 Wireless headphones are designed for long-term comfort to match their impressive battery life of up to 22 hours for all-day play. The soft over-ear cushions feature advanced venting and signature ergonomic pivoting for a custom, flexible fit for any head shape. The holistic design is lightweight, durable and comfortable, so you can get lost in your music.",
+title:"Beats Studio3 Wireless Headphones",
+
+desOne:"The Beats Studio3 Wireless headphones are designed for long-term comfort to match their impressive battery life of up to 22 hours for all-day play. The soft over-ear cushions feature advanced venting and signature ergonomic pivoting for a custom, flexible fit for any head shape. The holistic design is lightweight, durable and comfortable, so you can get lost in your music.",
+
 logo:"logo.svg",
+
 poster:"poster_img.png",
+
 desTwo:"  Take off. Beats Studio3 Wireless – Skyline Collection features four luxury colours to elevate every step of your journeys: Midnight Black, Crystal Blue, Desert Sand and Shadow Grey. Explore the world in style and enjoy a premium listening experience with Pure Adaptive Noise Cancelling (Pure ANC), which blocks external noise and preserves the full clarity, range and emotion of your sound. Up to 22 hours of battery life lets youenjoy non-stop music in comfort, even on long flights.",
+
 titleTwo:"The Beats Skyline Collection Shadow Grey",
 
 midImg: "wire_img.png",
+
 productInfo: "Product Information",
+
 bottomImg: "bottom_img.png",
+
 price: "$399.95",
+
 priceInfo: "3.5 out of 5 stars Colour - Shadow Grey Crystal BlueCrystal Blue Desert SandDesert Sand Midnight BlackMidnight Black",
+
 button: "Add to bag",
+
 copyright:"2019 BeatsStudio3.com",
 
 
