393b4216b1b35db1019400ce82b0a997fa4252d0
Jeril George Kadavan <jerilgkadavan@gmail.com>
2019-03-19 15:44:41 -0400

    first data

---
+++ data.js
@@ -0,0 +1,9 @@
+const data = {
+
+    title:"Beats Studio3 Wireless Headphones",
+    desOne:"The Beats Studio3 Wireless headphones are designed for long-term comfort to match their impressive battery life of up to 22 hours for all-day play. The soft over-ear cushions feature advanced venting and signature ergonomic pivoting for a custom, flexible fit for any head shape. The holistic design is lightweight, durable and comfortable, so you can get lost in your music.",
+logo:"logo.svg",
+poster:"poster_img.png"
+}
+
+module.exports = data;
